779753d9e184e1db6da97b6ec34815d9073101d7
Alexandr Shiriaev <alexandrshiriaev7@gmail.com>
2024-11-04 15:03:56 +0300

    chore: add nuxt ui module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+    modules: ['@nuxt/eslint', '@nuxt/ui'],
 });
